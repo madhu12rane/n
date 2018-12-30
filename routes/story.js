2dc6fb03e75a1e4a5b764f69baf8f2da302fa9e5
madhu12rane <45754127+madhu12rane@users.noreply.github.com>
2018-12-30 16:15:07 +0530

    Update story.js

--- routes/story.js
+++ routes/story.js
@@ -1,4 +1,4 @@
-var mongoose = require('./mongoose');
+var mongoose = require('mongoose');
 var Story = mongoose.model('Story');
 exports.stories=function(req,res){
 Story.find({}, function(err,stories){
